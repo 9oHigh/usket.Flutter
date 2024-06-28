e976e49af4dccdaedbda51dbd4773ee49a9304eb
9oHigh <wkdska957@naver.com>
2024-06-28 16:00:50 +0900

    feat: 백그라운드 컬러, 데코레이션

--- dice_project/lib/main.dart
+++ dice_project/lib/main.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-      home: Scaffold(body: Center(child: Text("Hello, world!!")))));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 74, 24, 159),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Colors.deepPurple,
+              Color.fromARGB(255, 78, 44, 138),
+              Color.fromARGB(255, 48, 26, 84)
+            ]),
+          ),
+          child: const Center(
+            child: Text("Hello, world!!"),
+          ),
+        ),
+      ),
+    ),
+  );
 }
