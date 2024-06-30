afaa9d0fb3bdf5b2c2c081bcbbedd136a626f0dd
9oHigh <wkdska957@naver.com>
2024-06-30 21:22:20 +0900

    feat: statelessWidget 클래스를 만들어 코드 가독성 높이기

--- dice_project/lib/main.dart
+++ dice_project/lib/main.dart
@@ -2,22 +2,39 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         backgroundColor: Color.fromARGB(255, 74, 24, 159),
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Colors.deepPurple,
-              Color.fromARGB(255, 78, 44, 138),
-              Color.fromARGB(255, 48, 26, 84)
-            ]),
-          ),
-          child: const Center(
-            child: Text("Hello, world!!"),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.deepPurple,
+                Color.fromARGB(255, 78, 44, 138),
+                Color.fromARGB(255, 48, 26, 84)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              "Hello, world!!",
+              style: TextStyle(color: Colors.white, fontSize: 28.5),
+            ),
+          ),
+        );
+  }
 }
